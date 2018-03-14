deadd7950246c9c5c7ec11dee40a91493601b74c
kalpavrikshika <vrikshikakalpa@gmail.com>
2018-03-14 15:11:58 +0000

    fix connection issue

--- Mqtt_leds.py
+++ Mqtt_leds.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
-def on_connect(client, userdata, rc):
-    #print ("Connected with rc: " + str(rc))
+def on_connect(client, userdata, flags, rc):
+    print ("Connected with rc: " + str(rc))
     client.subscribe("paho/demo/led")
 
 def on_message(client, userdata, msg):
-    #print ("Topic: "+ msg.topic+"\nMessage: "+str(msg.payload))
+    print ("Topic: "+ msg.topic+"\nMessage: "+str(msg.payload))
     if "white" in msg.payload:
         #print("  Green on!")
         GPIO.output(13, True)
@@ -27,9 +27,9 @@ def on_message(client, userdata, msg):
         GPIO.output(12, False)
 
 username = "paho"
-password = "pahopassword"     
-client.username_pw_set(username, password=pahopassword)
+password_= "pahopassword"
 client = mqtt.Client("raspberrypi")
+client.username_pw_set(username, password=password_)
 client.on_connect = on_connect
 client.on_message = on_message
 
