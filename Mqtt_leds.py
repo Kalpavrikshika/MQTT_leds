b1948b2f552da1942dceec7b91e57bdbbd7cc6b5
unknown <vrikshikakalpa@gmail.com>
2018-03-15 11:32:07 +0300

    fixes

--- Mqtt_leds.py
+++ Mqtt_leds.py
@@ -3,31 +3,31 @@ import RPi.GPIO as GPIO
 
 def on_connect(client, userdata, flags, rc):
     print ("Connected with rc: " + str(rc))
-    client.subscribe("paho/demo/led")
+    client.subscribe("<username>/demo/led")
 
 def on_message(client, userdata, msg):
     print ("Topic: "+ msg.topic+"\nMessage: "+str(msg.payload))
     if "white" in msg.payload:
-        #print("  Green on!")
+        #print("  White on!")
         GPIO.output(13, True)
     else:
-        #print("  Green off!")
+        #print("  White off!")
         GPIO.output(13, False)
     if "green" in msg.payload:
-        #print("  Yellow on!")
+        #print("  Green on!")
         GPIO.output(11, True)
     else:
-        #print("  Yellow off!")
+        #print("  Green off!")
         GPIO.output(11, False)
     if "blue" in msg.payload:
-        #print("  Red on!")
+        #print("  Blue on!")
         GPIO.output(12, True)
     else:
-        #print("  Red off!")
+        #print("  Blue off!")
         GPIO.output(12, False)
 
-username = "paho"
-password_= "pahopassword"
+username = "<username>"
+password_= "<password>"
 client = mqtt.Client("raspberrypi")
 client.username_pw_set(username, password=password_)
 client.on_connect = on_connect
